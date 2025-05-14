8cd44348a45997fa7ad97d52e7f55418d13412c2
Djeanql <djeanql@tutamail.com>
2025-05-14 21:10:28 +0100

    Convert block hash to big int before compasiron with target

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,18 @@
+use hex;
+use num_bigint::BigUint;
 use crate::block::Block;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+
+
+fn hash_less_than_target(hash: &str, target: &str) -> bool {
+    let hash_num = BigUint::from_bytes_be(&hex::decode(hash).unwrap());
+    let target_num = BigUint::from_bytes_be(&hex::decode(target).unwrap());
+    hash_num < target_num
+}
+
 pub fn mine(block: &mut Block) {
-    while block.digest > block.target {
+    while !hash_less_than_target(&block.digest, &block.target) {
         block.update_nonce_and_timestamp();
     }
 }
